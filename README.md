027ede35235a572f3463f7c952a3764101034ac8
Ade Firmansyah <ade.f@mail.ugm.ac.id>
2021-09-26 18:15:00 +0700

    Clean

--- README.md
+++
@@ -1,2 +0,0 @@
-# paw-mern-crud
- 
--- config/db.js
+++
@@ -1,6 +0,0 @@
-// Set the connection string based from the config vars of the production server
-// To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
-
-module.exports = {
-  db: "mongodb+srv://crud_paw:tes12345@cluster0.lt1i1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-};
